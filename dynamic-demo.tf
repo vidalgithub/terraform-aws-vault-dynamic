37ef2f73f00f1e52bd059161f390e65009830612
vidalgithub <88409846+vidalgithub@users.noreply.github.com>
2024-08-10 23:49:52 -0500

    Update dynamic-demo.tf

--- dynamic-demo.tf
+++ dynamic-demo.tf
@@ -58,7 +58,7 @@ data "aws_ami" "ubuntu" {
 # provisdion and EC2 instance
 resource "aws_instance" "aws-admin-vaut" {
   ami           = data.aws_ami.ubuntu.id
-  instance_type = "t2.micro"
+  instance_type = "t3.micro"
 
   tags = {
     Name = "aws-admin-vaut"
